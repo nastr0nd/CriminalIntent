1f851ead35a55daab87ca35c4a3bfb59cd8184a4
nastr0nd <lissandro.ferdinand@gmail.com>
2023-12-07 14:11:44 +0200

    View types task completed

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -3,7 +3,9 @@ package com.example.criminalintent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -21,10 +23,6 @@ class CrimeListFragment: Fragment() {                                           
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
@@ -48,7 +46,12 @@ class CrimeListFragment: Fragment() {                                           
         crimeRecyclerView.adapter = adapter
     }
 
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {                // Внутренний класс, представляющий элемент списка в RecyclerView
+    open class Holder(view:View): RecyclerView.ViewHolder(view) {
+        open fun bind (crime: Crime) {
+        }
+    }
+
+    private inner class CrimeHolder(view: View) : Holder(view), OnClickListener {                // Внутренний класс, представляющий элемент списка в RecyclerView
 
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
@@ -59,7 +62,7 @@ class CrimeListFragment: Fragment() {                                           
             itemView.setOnClickListener(this)
         }
 
-        fun bind(crime: Crime) {                                                                                        // Заполняет данные элемента списка данными из объекта Crime
+        override fun bind(crime: Crime) {                                                                                        // Заполняет данные элемента списка данными из объекта Crime
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
@@ -76,14 +79,56 @@ class CrimeListFragment: Fragment() {                                           
         }
     }
 
-    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {                    // Адаптер, управляющий данными для RecyclerView
+    private inner class PoliceCrimeHolder(view:View): Holder(view), OnClickListener {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {                                 // Создаётся новый CrimeHolder для каждого элемента списка
-            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
-            return CrimeHolder(view)
+        private val contactPoliceButton: Button = itemView.findViewById(R.id.contactPoliceButton)
+        private lateinit var crime: Crime
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+            contactPoliceButton.setOnClickListener{
+                Toast.makeText(context, "Calling 911!", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
-        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {                                               // Передаются данные из списка для конкретной позиции в CrimeHolder
+        override fun bind(crime: Crime) {                                                                                        // Заполняет данные элемента списка данными из объекта Crime
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<Holder>() {                    // Адаптер, управляющий данными для RecyclerView
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {                                 // Создаётся новый CrimeHolder для каждого элемента списка
+            return when (viewType) {
+                NORMAL_VIEW_TYPE -> {
+                    val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+                    CrimeHolder(view)
+                }
+                else -> {
+                    val view = layoutInflater.inflate(R.layout.list_item_crime_police, parent, false)
+                    PoliceCrimeHolder(view)
+                }
+            }
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {                                               // Передаются данные из списка для конкретной позиции в CrimeHolder
             val crime = crimes[position]
             holder.bind(crime)
         }
@@ -91,12 +136,24 @@ class CrimeListFragment: Fragment() {                                           
         override fun getItemCount(): Int {
             return crimes.size
         }
+
+        override fun getItemViewType(position: Int): Int {
+            val crime = crimes[position]
+            return if (crime.requiresPolice) {
+                POLICE_VIEW_TYPE
+            } else {
+                NORMAL_VIEW_TYPE
+            }
+        }
+
     }
 
     companion object {
         fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
         }
+        private const val NORMAL_VIEW_TYPE = 0
+        private const val POLICE_VIEW_TYPE = 1
     }
 }
 
