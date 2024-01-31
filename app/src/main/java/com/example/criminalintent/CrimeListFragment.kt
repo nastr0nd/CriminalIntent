d6472c9d0ab044858bffaead191a55316788d47d
bsasha <lissandro.ferdinand@gmail.com>
2024-01-31 12:41:09 +0200

    Chapter 11 completed

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,33 +19,37 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {                                                                                   // Фрагмент списка преступлений
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = null
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-        ): View? {
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
-
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
-
+        crimeRecyclerView.adapter = adapter
         return view
     }
 
-    private fun updateUI() {                                                                                            // Инициализация адаптера и установка его в RecyclerView
-        val crimes = crimeListViewModel.crimes
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+    }
+
+    private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
@@ -99,8 +105,3 @@ class CrimeListFragment: Fragment() {                                           
         }
     }
 }
-
-
-// CrimeListFragment отображает список преступлений в вертикальном списке с использованием RecyclerView.
-// Каждый элемент списка представлен вложенным классом CrimeHolder, который содержит информацию о заголовке и дате преступления.
-// Адаптер CrimeAdapter управляет данными для каждого элемента списка, а CrimeListViewModel служит для хранения данных о преступлениях.
--- app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -5,8 +5,8 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.criminalintent.Crime
 
-@Database(entities = [ Crime::class ], version = 1)
-@TypeConverters(CrimeTypeConverters::class)                          // Явное добавление конвертеров к классу базы данных.
+@Database(entities = [ Crime::class ], version = 1, exportSchema = false)
+@TypeConverters(CrimeTypeConverters::class)                                                                  // Явное добавление конвертеров к классу базы данных.
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
 }
